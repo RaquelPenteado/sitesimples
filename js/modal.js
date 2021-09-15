8c232cb248dc9e67b07ec5616bfa3a7a17a0ea32
Raquel Penteado <raquelpenteado@yahoo.com.br>
2021-09-15 17:43:20 -0300

    first commit

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+function modalGo() {
+    let Modal = document.querySelector('.modal-wrapper')
+
+    Modal.classList.add('active')
+}
+
+function closeModal() {
+    let Modal = document.querySelector('.modal-wrapper')
+
+    Modal.classList.remove('active')
+
+}
